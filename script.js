668e817a941cd3ef82becc42680c2e06e503f8ae
Sikha Babu <sikhababu27@gmail.com>
2024-11-13 11:25:48 +0530

    Added index.html and script.js - DOM examples

---
+++ script.js
@@ -0,0 +1,23 @@
+let element1 = document.getElementById("main-title")
+console.log(element1)
+
+let element2 = document.getElementsByClassName("container")
+console.log(element2)
+
+let element3 = document.getElementsByClassName("section")
+console.log(element3)
+
+let element4 = document.getElementsByTagName("p")
+console.log(element4)
+
+let element5 = document.querySelector(".btn")
+console.log(element5)
+
+let element6 = document.querySelector("section")
+console.log(element6)
+
+let element7 = document.querySelectorAll(".btn")
+console.log(element7)
+
+let element8 = document.querySelectorAll(".container > section > p")
+console.log(element8)
